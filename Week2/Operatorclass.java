c7b7ad6a0a6b19c560208272ecb08c3ad98a2fe7
Nishan Bista <nishanbista525@gmail.com>
2024-04-19 14:40:27 +0545

    Operatorsclass

---
+++ Week2/Operatorclass.java
@@ -0,0 +1,42 @@
+package Week2;
+
+public class Operatorclass {
+    public static void main(String[] args) {
+
+        /* Arithemetic Operators */
+
+        int aNum1 = 10;
+        int aNum2 = 20;
+        System.out.println("the sum is: " + " " + (aNum1 + aNum2));
+        System.out.println("Subtraction is: " + " " + (aNum1 - aNum2));
+        System.out.println("Multiplication is: " + " " + (aNum1 * aNum2));
+        System.out.println("Division is " + " " + (aNum1/aNum2));
+        System.out.println("Modulo is :"+" "+(aNum1%aNum2));
+
+
+
+        /* Assignment Operators */
+        int asNum = 20;
+        System.out.println("+=10 is " + (asNum+=10));
+        System.out.println("-=10 is "+ (asNum-=10));
+        System.out.println("*=10 is " + (asNum*=10));
+        System.out.println("/=10 is " + (asNum/=10));
+        System.out.println("%=2 is " + (asNum%=2));
+    }
+
+    /*
+        
+        Relational Operators
+
+
+    */  
+
+
+    /*
+        Logial Operators
+
+
+    */
+    
+
+}
---
+++ Week2/home.java
@@ -0,0 +1,12 @@
+package Week2;
+
+public class home {
+    public static void main(String[] args) {
+
+        // final int static Var
+
+
+
+    }
+    
+}
